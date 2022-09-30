6cb4cdcf5ca3acfa3b3181fceb0d269e996ca8f0
James Lipe <james@jameslipe.com>
2022-09-29 19:09:01 -0500

    hello world react deployment

---
+++ infrastructure/lib/stacks/frontendStack.ts
@@ -0,0 +1,34 @@
+import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib'
+import { Distribution, OriginAccessIdentity } from 'aws-cdk-lib/aws-cloudfront'
+import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins'
+import { Bucket, BucketAccessControl } from 'aws-cdk-lib/aws-s3'
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment'
+import { Construct } from 'constructs'
+
+export class FrontendStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props)
+
+    const bucket = new Bucket(this, 'frontendBucket', {
+      accessControl: BucketAccessControl.PRIVATE,
+    })
+
+    new BucketDeployment(this, 'BucketDeployment', {
+      destinationBucket: bucket,
+      sources: [Source.asset('../frontend/build')],
+    })
+
+    const originAccessIdentity = new OriginAccessIdentity(
+      this,
+      'OriginAccessIdentity'
+    )
+    bucket.grantRead(originAccessIdentity)
+
+    const distribution = new Distribution(this, 'Distribution', {
+      defaultRootObject: 'index.html',
+      defaultBehavior: {
+        origin: new S3Origin(bucket, { originAccessIdentity }),
+      },
+    })
+  }
+}
