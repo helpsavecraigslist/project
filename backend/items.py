cb7706aa5f94c906c412b651853d21a4cd1851db
James Lipe <james@jameslipe.com>
2022-10-16 17:09:53 -0500

    Items database added. Basic lambda function responding to requests

--- backend/items.py
+++ backend/items.py
@@ -1,18 +1,40 @@
 import json
+import boto3
+import os
 
 
 def lambda_handler(event, context):
+    print("event")
     print(event)
+    print("context")
     print(context)
-    return {
+
+    def build_success_response(data):
+      return {
         'statusCode': 200,
         'headers': {
             'Content-Type': 'application/json',
             'Access-Control-Allow-Headers': '*, Authorization',
             'Access-Control-Allow-Origin': '*'
         },
-        'body': json.dumps({
-            'message': 'hello from items'
-        }),
+        'body': json.dumps(data),
         "isBase64Encoded": False
     }
+
+    dynamodb = boto3.resource('dynamodb')
+
+    table = dynamodb.Table(os.environ['ITEMS_TABLE'])
+
+    avaliable_locations = ["Corvallis", "San Diego", "Austin", "Online"]
+    if event['path'] == '/items/locations':
+      return build_success_response(avaliable_locations)
+
+    if event['httpMethod'] == 'GET':
+      items = table.scan()
+      return build_success_response(items)
+    
+    if event['httpMethod'] == 'POST':
+      body = event['body']
+      print(body)
+      return build_success_response(event['requestContext']['authorizer']['claims'])
+
--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Auth, Hub } from 'aws-amplify'
+import { Auth, API } from 'aws-amplify'
 import '@aws-amplify/ui-react/styles.css'
 import { CognitoIdToken } from 'amazon-cognito-identity-js'
 
@@ -8,12 +8,46 @@ interface ProfileProps {
 }
 
 const Profile = (props: ProfileProps) => {
+  const postData = async () => {
+    const apiName = 'default'
+    const path = 'items'
+    const myInit = {
+      headers: {
+        Authorization: `Bearer ${props.user.getJwtToken()}`,
+      },
+      body: {
+        hello: 'there',
+      },
+    }
+
+    return await API.post(apiName, path, myInit)
+  }
+
+  const getData = async () => {
+    const apiName = 'default'
+    const path = 'items'
+    const myInit = {}
+
+    return await API.get(apiName, path, myInit)
+  }
+
+  const getLocations = async () => {
+    const apiName = 'default'
+    const path = 'items/locations'
+    const myInit = {}
+
+    return await API.get(apiName, path, myInit)
+  }
+
   return (
     <>
       <h1>
         Here is your token decode it <a href="http://jwt.io">here</a>
       </h1>
       <p>{props.user.getJwtToken()}</p>
+      <button onClick={() => postData()}>Post something</button>
+      <button onClick={() => getLocations()}>Get Locations</button>
+      <button onClick={() => getData()}>Get all items</button>
     </>
   )
 }
