c796ba8eec0bbf2c8bf6fc4536c513ca16de3b81
Yujun Liu <movaxbxjmp@gmail.com>
2022-11-03 01:20:10 -0500

    messages cognito. react handler

--- frontend/src/components/MessageNewMessage.tsx
+++ frontend/src/components/MessageNewMessage.tsx
@@ -1,6 +1,6 @@
-import { useEffect } from 'react'
+import { FormEvent, useEffect, useState } from 'react'
 import Box from '@mui/material/Box'
-import { API } from 'aws-amplify'
+import { Auth, API } from 'aws-amplify'
 import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
 import { useNavigate, useLocation } from 'react-router-dom'
@@ -8,8 +8,10 @@ import Typography from '@mui/material/Typography';
 
 
 export default function MessageNewMessage() {
-    const navigate = useNavigate()
-    const itemData = useLocation()
+  const navigate = useNavigate()
+  const itemData = useLocation()
+  const [content, setContent] = useState('')
+  const [redirect, setRedirect] = useState(false)
 
   const fetchMessageNewMessage = async () => {
     const apiName = 'default'
@@ -27,6 +29,44 @@ export default function MessageNewMessage() {
     fetchMessageNewMessage()
     
   }, [])
+
+  const postNewChat = async () => {
+    const apiName = 'default'
+    const path = 'messages/newchat'
+    const myInit = {
+      headers: {
+        Authorization: `Bearer ${(await Auth.currentSession())
+          .getIdToken()
+          .getJwtToken()}`,
+      },
+      body: {
+        to_user: itemData.state.userID,
+        subject:itemData.state.subject,
+        content,
+      },
+    }
+
+    // TODO - add better error handling for unsuccessful post
+    try {
+      return await API.post(apiName, path, myInit)
+    } catch {
+      console.error('Error posting item')
+    }
+  }
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault()
+    const response = await postNewChat()
+    // TODO
+    // if (redirect) {
+      // const itemUrl = generateItemUrl(
+      //   response.Item['UserID'],
+      //   response.Item['PostedDate']
+      // )
+      // navigate(itemUrl)
+    // }
+  }
+
   return (
     <>
     {/* 
@@ -61,20 +101,21 @@ export default function MessageNewMessage() {
           
                 <TextField
                     id="outlined-multiline-static"
-                    
+                    label='Content'
                     multiline
                     rows={4}
                     sx = {{my:.5}}
                     placeholder = "enter your message here"
                     fullWidth
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
                 />
                 
             </Box>
           <Button 
             variant="contained"
             sx = {{my:.5}}
-            onClick={() =>
-                navigate('/MessageDetail')}
+            onClick={handleSubmit}
             >Start Messaging
           </Button>
           </Box>
